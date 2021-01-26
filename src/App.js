29759739866555a0b37b3363e1674c73c5605bd4
Spanache Ramona <spanache.ramona@gmail.com>
2021-01-26 19:00:44 +0200

    Add all my files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Route, BrowserRouter as Router, Switch, Link } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Login from "./Pages/Login";
+import ToDo from "./Pages/ToDo";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+            <Link to="/login">Login</Link>
+            </li>
+            <li>
+            <Link to="/todo">ToDo</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/about" component={About} />
+          <Route path="/login" component={Login} />
+          <Route path="/todo" component={ToDo} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Pages/ToDo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ToDo() {
+    return (
+        <div>
+            <h1>ToDo</h1>
+        </div>
+    );
+}
